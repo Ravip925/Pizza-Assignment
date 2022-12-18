ab595ac4bd9588af52e949b58841f677dcfc51fa
“Ravi <patilravi400@gmail.com>
2022-12-18 10:41:41 +0530

    commit

--- pizza.js
+++ pizza.js
@@ -1,27 +1,29 @@
 const draggableElement1 = document.getElementById("draggable-element1");
 const draggableElement2 = document.getElementById("draggable-element2");
+const dropZone = document.getElementById("dropzone");
 const lists = document.querySelectorAll(".list");
 const greet = document.querySelector(".greet");
+const ingredient_title = document.querySelector('.ingredient-title')
 
 draggableElement1.addEventListener("dragover", function (e) {
   e.preventDefault();
   setTimeout(() => {
-    document.getElementById("dropzone").classList.add("add1");
-    document.getElementById("dropzone").classList.remove("add2");
-    document.getElementById("dropzone").classList.remove("pizza1");
-    document.getElementById("dropzone").classList.remove("pizza2");
-    document.getElementById("dropzone").classList.remove("pizza3");
+    dropZone.classList.add("add1");
+    dropZone.classList.remove("add2");
+    dropZone.classList.remove("pizza1");
+    dropZone.classList.remove("pizza2");
+    dropZone.classList.remove("pizza3");
   }, 1000);
 });
 
 draggableElement2.addEventListener("dragover", function (e) {
   e.preventDefault();
   setTimeout(() => {
-    document.getElementById("dropzone").classList.add("add2");
-    document.getElementById("dropzone").classList.remove("add1");
-    document.getElementById("dropzone").classList.remove("pizza1");
-    document.getElementById("dropzone").classList.remove("pizza2");
-    document.getElementById("dropzone").classList.remove("pizza3");
+    dropZone.classList.add("add2");
+    dropZone.classList.remove("add1");
+    dropZone.classList.remove("pizza1");
+    dropZone.classList.remove("pizza2");
+    dropZone.classList.remove("pizza3");
   }, 1000);
 });
 
@@ -30,9 +32,9 @@ lists.forEach((element, index) => {
     element.addEventListener("dragover", function (e) {
       e.preventDefault();
       setTimeout(() => {
-        document.getElementById("dropzone").classList.add("pizza1");
-        document.getElementById("dropzone").classList.remove("pizza2");
-        document.getElementById("dropzone").classList.remove("pizza3");
+        dropZone.classList.add("pizza1");
+        dropZone.classList.remove("pizza2");
+        dropZone.classList.remove("pizza3");
         greet.style.visibility = "visible";
       }, 1000);
     });
@@ -40,9 +42,9 @@ lists.forEach((element, index) => {
     element.addEventListener("dragover", function (e) {
       e.preventDefault();
       setTimeout(() => {
-        document.getElementById("dropzone").classList.add("pizza2");
-        document.getElementById("dropzone").classList.remove("pizza1");
-        document.getElementById("dropzone").classList.remove("pizza3");
+        dropZone.classList.add("pizza2");
+        dropZone.classList.remove("pizza1");
+        dropZone.classList.remove("pizza3");
         greet.style.visibility = "visible";
       }, 1000);
     });
@@ -50,21 +52,23 @@ lists.forEach((element, index) => {
     element.addEventListener("dragover", function (e) {
       e.preventDefault();
       setTimeout(() => {
-        document.getElementById("dropzone").classList.add("pizza3");
-        document.getElementById("dropzone").classList.remove("pizza2");
-        document.getElementById("dropzone").classList.remove("pizza1");
+        dropZone.classList.add("pizza3");
+        dropZone.classList.remove("pizza2");
+        dropZone.classList.remove("pizza1");
         greet.style.visibility = "visible";
+        
       }, 1000);
     });
   }
 });
 
-const dropzone = document.getElementById("dropzone");
-dropzone.addEventListener("dragover", function (event) {
+
+dropZone.addEventListener("dragover", function (event) {
   event.preventDefault();
 });
 
-dropzone.addEventListener("drop", function (event) {
+dropZone.addEventListener("drop", function (event) {
   event.preventDefault();
   document.getElementById("ul").style.visibility = "visible";
+  ingredient_title.style.visibility = "visible";
 });
